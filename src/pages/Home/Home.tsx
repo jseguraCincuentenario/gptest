f63812919e1ba0bb1a9c41e76c8861d93a81e15c
jseguraGrupoCinc <jsegura@cincuentenario.com>
2024-06-02 11:18:53 -0500

    Configurar tabla

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
 import { People } from '@/data/people';
+import { Person } from '@/models/people';
+import { Checkbox } from '@mui/material';
 
 
 export type HomeProps = {
@@ -9,14 +11,35 @@ export type HomeProps = {
 
 const Home: React.FC<HomeProps>  = ({}) => {
 
-	// const pageSize = 5
+	const [selectedPeople, setSelectedPeople] = useState<Person[]>([])
+	const pageSize = [5,10]
+
+	const findPerson = (person: Person) => !!selectedPeople.find(p => p.id === person.id)
+	const filterPerson = (person: Person) => selectedPeople.filter(p => p.id !== person.id)
+
+	const handleChange = (person: Person) => {
+		setSelectedPeople(findPerson(person) ? filterPerson(person) : [...selectedPeople, person])
+	}
 	
-	const columnas = [{
-		field: 'name',
-		headerName: 'Nombre',
-		flex: 1,
-		minwidth: 150,
-		renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+	const columnas = [
+		{
+			field: 'actions',
+			type: 'actions',
+			sortable: false,
+			headerName: '',
+			width: 50,
+			renderCell: (params: GridRenderCellParams) => <>{
+				<Checkbox size='small' checked={findPerson(params.row)}
+					onChange={() => handleChange(params.row)}
+				/>
+			}</>
+		},
+		{
+			field: 'name',
+			headerName: 'Nombre',
+			flex: 1,
+			minwidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
 		},
 		{
 			field: 'category',
@@ -24,21 +47,31 @@ const Home: React.FC<HomeProps>  = ({}) => {
 			flex: 1,
 			minwidth: 150,
 			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+		},
+		{
+			field: 'company',
+			headerName: 'Empresa',
+			flex: 1,
+			minwidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>
 		}
 	]
 
 	return (
-		<div>
- 			<DataGrid
-				rows={People}
-				columns={columnas}
-				disableColumnSelector
-				disableRowSelectionOnClick
-				autoHeight
-				pageSizeOptions={[1,1,1]}
-
-			/>
- 		</div>
+		<DataGrid
+			rows={People}
+			columns={columnas}
+			disableColumnSelector
+			disableRowSelectionOnClick
+			autoHeight
+			pageSizeOptions={pageSize}
+			initialState={{
+				pagination: {
+				  paginationModel: { pageSize: 5, page: 0 },
+				},
+			}}
+			getRowId={(row: any) => row.id}
+		/>
 	);
 };
 
